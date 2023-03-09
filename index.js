30ac3b84686145e1d0dfa0171a0df4cdaa6b94a0
DesmondDike <121991195+DesmondDike@users.noreply.github.com>
2023-03-09 14:54:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let countEl = document.getElementById("count-ed")
+let savedEl = document.getElementById("save-btn")
+let count = 0
+
+function register()
+count = count + 1
+countEl.textContent = count
